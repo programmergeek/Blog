0b74cf799b59ea9b7d1ba290ff231e9ba54c90f3
Will <programergeek@gmail.com>
2022-12-18 13:22:09 +0200

    setup contenful

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "contentful";
 
-type Data = {
-  name: string
-}
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const client = createClient({
+    space: "p8y3eldp3r1s",
+    accessToken: "hhHzyqlJVFuVo1VMTg2e5nG9XQhvtzRiSF9z-rglG3s",
+  });
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    client
+      .getEntry("mLWTEKibiJ7HQlVnWrOBP")
+      .then((entry) => entry)
+      .catch(console.error)
+  );
 }
