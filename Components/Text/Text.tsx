6ea83fc038ba62a916204f6eba83d4497281a766
Will <programergeek@gmail.com>
2022-12-19 10:52:17 +0200

    use css to contol font instead of nextjs api

--- Components/Text/Text.tsx
+++ Components/Text/Text.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Lato } from "@next/font/google";
 
 interface Props extends React.HTMLAttributes<HTMLParagraphElement> {
   children: React.ReactNode;
 }
 
-const lato = Lato({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 export const Text: React.FC<Props> = ({ ...props }) => {
-  return (
-    <p className={lato.className} {...props}>
-      {props.children}
-    </p>
-  );
+  return <p {...props}>{props.children}</p>;
 };
