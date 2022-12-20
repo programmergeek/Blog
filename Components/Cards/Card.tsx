d0d2c833e812832e3a16607d6d2dadca23e9f176
Will <programergeek@gmail.com>
2022-12-20 08:56:46 +0200

    rework card component

--- Components/Cards/Card.tsx
+++ Components/Cards/Card.tsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { Card, styled, CardProps } from "@nextui-org/react";
-
-const Custom = styled(Card, {
-  variant: {
-    flat: {
-      maxWidth: 700,
-      height: 500,
-      border: "2px solid black",
-      borderRadius: 0,
-      backgroundColor: "White",
-    },
-  },
-});
+import { Card, CardProps } from "@nextui-org/react";
 
 export const Crd: React.FC<CardProps> = ({ ...props }) => {
   return (
-    <Custom {...props} variant="flat">
+    <Card
+      variant="flat"
+      css={{
+        border: "2px solid black",
+        borderRadius: 0,
+        backgroundColor: "#fff",
+        paddingTop: 25,
+        paddingLeft: 10,
+        paddingRight: 10,
+      }}
+      {...props}
+    >
       {props.children}
-    </Custom>
+    </Card>
   );
 };
