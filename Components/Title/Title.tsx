49e203367b4158e8e0fb05896fac3ad178d03fd8
Will <programergeek@gmail.com>
2022-12-23 13:28:11 +0200

    make font size responsive

--- Components/Title/Title.tsx
+++ Components/Title/Title.tsx
@@ -8,8 +8,14 @@ export const Title: React.FC<TextProps> = ({ css, ...props }) => {
       css={{
         fontFamily: "Teko",
         fontWeight: 400,
-        fontSize: 32,
+        fontSize: 40,
         letterSpacing: ".005em",
+        "@md": {
+          fontSize: "3.5rem",
+        },
+        "@xl": {
+          fontSize: "4.5rem",
+        },
         ...css,
       }}
       {...props}
