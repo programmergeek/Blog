53c25c601d9efcb0900ba22feb040f2741650549
Will <programergeek@gmail.com>
2022-12-23 12:20:36 +0200

    move styles for title component to the component

--- Components/Title/Title.tsx
+++ Components/Title/Title.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Text } from "@nextui-org/react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const Title: React.FC<Props> = ({ ...props }) => {
-  return <h1> {props.children} </h1>;
+  return (
+    <Text
+      h1
+      css={{
+        fontFamily: "Teko",
+        fontWeight: 400,
+        fontSize: 32,
+        letterSpacing: ".005em",
+      }}
+    >
+      {props.children}
+    </Text>
+  );
 };
