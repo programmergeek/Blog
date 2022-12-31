ddd780a8ec96320405933c67ae4f318619e1a623
Will <programergeek@gmail.com>
2022-12-31 21:02:31 +0200

    create setup file for contentful client

---
+++ Contentful/setup.ts
@@ -0,0 +1,7 @@
+import { createClient } from "contentful";
+
+export const client = createClient({
+  space: process.env.SPACE_ID as string,
+  accessToken: process.env.ACCESS_TOKEN as string,
+  environment: "master",
+});
