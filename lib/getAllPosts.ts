1174ae4f3890b037e438d2cd92a8c9c9b6d03ab2
Will <programergeek@gmail.com>
2023-01-01 18:48:36 +0200

    create a method to retrieve all posts

---
+++ lib/getAllPosts.ts
@@ -0,0 +1,20 @@
+import { client } from "../Contentful/setup";
+import { extractPosts } from "./extractPosts";
+
+/**
+ * A function that returns all posts
+ */
+export const getAllPosts = async (limit = 8, skip = 0) => {
+  let posts;
+  await client
+    .getEntries({
+      content_type: "post",
+      limit: limit,
+      skip: skip,
+    })
+    .then((entreies) => {
+      posts = extractPosts(entreies);
+    });
+
+  return posts;
+};
