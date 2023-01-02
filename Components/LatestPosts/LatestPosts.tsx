e109e1f7e9472bdb70b938a641dff13bcde0eb3c
Will <programergeek@gmail.com>
2023-01-02 11:49:12 +0200

    get data from the backend

--- Components/LatestPosts/LatestPosts.tsx
+++ Components/LatestPosts/LatestPosts.tsx
@@ -1,45 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { Header, Post } from "..";
+import { Header, Post, PostGrid } from "..";
 import { Pagination } from "@nextui-org/react";
 import styles from "./LatestPosts.module.css";
 
 export const LatestPosts: React.FC = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState<any>([]);
+  const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 10;
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      });
+  }, [currentPage]);
 
   return (
     <div>
       <div style={{ paddingTop: 50, paddingBottom: 50 }}>
         <Header>Latest</Header>
       </div>
-      <div className={styles.latest}>
-        <Post
-          category="Web Dev"
-          date="12/04/2022"
-          imgSrc="/images/gunsligner.jpg"
-          link="https://google.com"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati voluptas accusantium voluptatibus, iste quasi est alias omnis ratione a natus unde et maxime sint, minus labore atque nulla dolor."
-          title="Title"
-        />
-        <Post
-          category="Web Dev"
-          date="12/04/2022"
-          imgSrc="/images/gunsligner.jpg"
-          link="https://google.com"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati voluptas accusantium voluptatibus, iste quasi est alias omnis ratione a natus unde et maxime sint, minus labore atque nulla dolor."
-          title="Title"
-        />
-        <Post
-          category="Web Dev"
-          date="12/04/2022"
-          imgSrc="/images/gunsligner.jpg"
-          link="https://google.com"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati voluptas accusantium voluptatibus, iste quasi est alias omnis ratione a natus unde et maxime sint, minus labore atque nulla dolor."
-          title="Title"
-        />
-      </div>
+      {loading ? (
+        <div style={{ fontFamily: "Teko", fontSize: 40, textAlign: "center" }}>
+          loading...
+        </div>
+      ) : (
+        <PostGrid posts={posts} />
+      )}
       <div
         style={{
           display: "flex",
