7885f610af0a0e0172a6a7b841dab5e36779b6df
Will <programergeek@gmail.com>
2023-01-04 05:17:17 +0200

    extend getAllPosts method to accept queries

--- lib/getAllPosts.ts
+++ lib/getAllPosts.ts
@@ -4,13 +4,14 @@ import { extractPosts } from "./extractPosts";
 /**
  * A function that returns all posts
  */
-export const getAllPosts = async (limit = 8, skip = 0) => {
+export const getAllPosts = async (limit = 8, skip = 0, query?: string) => {
   let posts;
   await client
     .getEntries({
       content_type: "post",
       limit: limit,
       skip: skip,
+      query: query,
     })
     .then((entreies) => {
       posts = extractPosts(entreies);
