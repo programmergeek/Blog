4c7c4b44a80a2b62835fa974b1249d841529acd4
Will <programergeek@gmail.com>
2023-01-04 20:12:17 +0200

    get data from server side props

--- pages/search/[page].tsx
+++ pages/search/[page].tsx
@@ -1,10 +1,25 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useState } from "react";
+import { getAllPosts } from "../../lib/getAllPosts";
 
-export default function Search() {
+export default function Search({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [results, updateResults] = useState<any>([]);
   const [loading, updateLoading] = useState(true);
   const [currentPage, updateCurrentPage] = useState(1);
   const [total, updateTotal] = useState(0);
 
-  return <p></p>;
+  return JSON.stringify(data);
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const page = parseInt(context.params?.page as string);
+  const search = context.params?.search as string;
+  const data = await getAllPosts(undefined, (page - 1) * 8, search as string);
+  return {
+    props: {
+      data,
+    },
+  };
+};
