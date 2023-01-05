6ce6a993ae5863e808f865a8c3adf5a826bc77f2
Will <programergeek@gmail.com>
2023-01-05 17:15:08 +0200

    implement pagination

--- pages/search/[page].tsx
+++ pages/search/[page].tsx
@@ -1,5 +1,7 @@
+import { Pagination } from "@nextui-org/react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { PostGrid } from "../../Components";
 import { getAllPosts } from "../../lib/getAllPosts";
 
@@ -7,13 +9,33 @@ export default function Search({
   data,
   total,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+  const { page, search } = router.query;
   const [totalPosts, updateTotalPosts] = useState(total);
   const [results, updateResults] = useState<any>(data);
-  const [currentPage, updateCurrentPage] = useState(1);
+  const [currentPage, updateCurrentPage] = useState(page);
+
+  // route user to the next page
+  const handleChange = (pageNum: number) => {
+    router.replace({
+      pathname: `/search/[page]`,
+      query: { search: search, page: pageNum },
+    });
+  };
+
+  useEffect(() => {
+    // update result data everytime we go to another page
+    updateResults(data);
+  }, [router.query.page]);
 
   return (
     <div>
-      <PostGrid posts={results} />
+      <PostGrid posts={results} key={router.asPath} />
+      <Pagination
+        total={Math.ceil(totalPosts / 8)}
+        initialPage={parseInt(currentPage as string)}
+        onChange={(pageNum) => handleChange(pageNum)}
+      />
     </div>
   );
 }
