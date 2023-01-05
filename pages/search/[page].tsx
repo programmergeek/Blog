10adaafa3c6fcce62954f60b8d47806e42c1301f
Will <programergeek@gmail.com>
2023-01-05 16:01:05 +0200

    restructure data returned from getServerSideProps

--- pages/search/[page].tsx
+++ pages/search/[page].tsx
@@ -1,24 +1,31 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useState } from "react";
+import { PostGrid } from "../../Components";
 import { getAllPosts } from "../../lib/getAllPosts";
 
 export default function Search({
   data,
+  total,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [results, updateResults] = useState<any>([]);
-  const [loading, updateLoading] = useState(true);
+  const [totalPosts, updateTotalPosts] = useState(total);
+  const [results, updateResults] = useState<any>(data);
   const [currentPage, updateCurrentPage] = useState(1);
-  const [total, updateTotal] = useState(0);
 
-  return JSON.stringify(data);
+  return (
+    <div>
+      <PostGrid posts={results} />
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { page, search } = context.query as any;
   const data = await getAllPosts(undefined, (page - 1) * 8, search as string);
+  const total = data.pop().total;
   return {
     props: {
       data,
+      total,
     },
   };
 };
