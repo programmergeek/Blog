be9f1683894663251bdd6a5edcd0d036fb3dc270
Will <programergeek@gmail.com>
2023-01-11 17:11:26 +0200

    add render options for code marks, heading1 and paragraph

--- Contentful/options.tsx
+++ Contentful/options.tsx
@@ -1,11 +1,27 @@
 import { Options } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 import { Text, Title } from "../Components";
 
 export const RenderOption: Options = {
   renderNode: {
+    [BLOCKS.HEADING_1]: (node, children) => {
+      return <Title> {children} </Title>;
+    },
     [BLOCKS.HEADING_2]: (node, children) => {
-      return <Title css={{ fontSize: 35 }}> {children} </Title>;
+      return <Title style={{ fontSize: 65 }}> {children} </Title>;
+    },
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      return <Text>{children}</Text>;
+    },
+  },
+  renderMark: {
+    [MARKS.CODE]: (text) => {
+      return (
+        <code style={{ color: "black", backgroundColor: "white" }}>
+          {" "}
+          {text}{" "}
+        </code>
+      );
     },
   },
 };
