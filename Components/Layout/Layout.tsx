22e510800a4622f8f43bcdec35715412f232d5b7
Will <programergeek@gmail.com>
2023-01-12 13:55:46 +0200

    add styles and restructure

--- Components/Layout/Layout.tsx
+++ Components/Layout/Layout.tsx
@@ -1,7 +1,27 @@
 import React, { HTMLAttributes } from "react";
+import { Footer } from "../Footer/Footer";
+import { Navbar } from "../Navbar/Navbar";
+import { SiteTitle } from "../SiteTitle";
 
 export const Layout: React.FC<HTMLAttributes<HTMLDivElement>> = ({
   ...props
 }) => {
-  return <div></div>;
+  return (
+    <div>
+      <main
+        style={{
+          maxWidth: 1440,
+          marginRight: "auto",
+          marginLeft: "auto",
+          paddingRight: 20,
+          paddingLeft: 20,
+        }}
+      >
+        <SiteTitle />
+        <Navbar />
+        {props.children}
+      </main>
+      <Footer />
+    </div>
+  );
 };
