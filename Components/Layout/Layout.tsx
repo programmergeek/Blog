147857ff5727bd5923362d531d3c0c9f9824712a
Will <programergeek@gmail.com>
2023-03-19 15:10:25 +0200

    style: redifine padding on the sides of the main content

--- Components/Layout/Layout.tsx
+++ Components/Layout/Layout.tsx
@@ -2,21 +2,14 @@ import React, { HTMLAttributes } from "react";
 import { Footer } from "../Footer/Footer";
 import { Navbar } from "../Navbar/Navbar";
 import { SiteTitle } from "../SiteTitle";
+import style from "../../styles/Layout.module.css";
 
 export const Layout: React.FC<HTMLAttributes<HTMLDivElement>> = ({
   ...props
 }) => {
   return (
     <div>
-      <main
-        style={{
-          maxWidth: 1440,
-          marginRight: "auto",
-          marginLeft: "auto",
-          paddingRight: 20,
-          paddingLeft: 20,
-        }}
-      >
+      <main className={style.layout}>
         <SiteTitle />
         <Navbar />
         {props.children}
